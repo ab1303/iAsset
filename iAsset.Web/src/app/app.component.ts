4efcee3486ed78d15b46459c469e25dd964a755b
Abdul Wahab <ab.1303@gmail.com>
2017-10-04 17:22:44 +1100

    Changes

--- iAsset.Web/src/app/app.component.ts
+++ iAsset.Web/src/app/app.component.ts
@@ -2,8 +2,8 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:'my-app',
-    template:`<h1>Hello {{name}} </h1>`
+    selector:'iasset-app',
+    templateUrl:'./app.component.html'
 })
 export class AppComponent {
     name = 'Angular';
